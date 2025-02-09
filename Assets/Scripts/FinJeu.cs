c575b849e327b8a7229378be74b26205718b8c10
GabbDup <gabriel.dupras01@gmail.com>
2025-02-09 10:21:13 -0500

    modification du parcours

--- Assets/Scripts/FinJeu.cs
+++ Assets/Scripts/FinJeu.cs
@@ -11,14 +11,28 @@ public class FinJeu : MonoBehaviour
     [SerializeReference] private GameObject _terminus;
     [SerializeReference] private GameObject _bille;
 
-    private int _time = 0;
+    float _time = 0f;
     
     /// <summary>
     /// OnTriggerEnter is called when the Collider other enters the trigger.
     /// </summary>
     /// <param name="other">The other Collider involved in this collision.</param>
-    private void OnTriggerEnter(Collider _terminus)
+    void OnTriggerEnter(Collider _terminus)
     {
-        
+        Debug.Log("I'm In!");
+        for(int i = 0; i<6; i++)
+        {
+            _time += Time.deltaTime*4;
+            print(_time);
+            if(_time == 5)
+            {
+                Redemarrer();
+            }
+        }
+    }
+
+    void Redemarrer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
